cdd51c439109e51b2405b67b03d7f101dcd080ef
PabloGallegosH <68388117+PabloGallegosH@users.noreply.github.com>
2020-07-16 13:58:00 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LGCovid.Startup))]
+namespace LGCovid
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
